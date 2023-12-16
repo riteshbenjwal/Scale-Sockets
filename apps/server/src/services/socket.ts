ba543ffcaf440c72ea548c22a4260f8d3b0cb028
riteshgeeky <ritesh.benjwal@geeky.dev>
2023-12-16 20:48:09 +0530

    Add Redis PUB SUB for handling multiserver

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,10 +1,24 @@
 import { Server } from "socket.io";
+import Redis from "ioredis";
+
+// const REDIS_URL =
+//   "redis://default:AVNS_vSuxiGWwVIoZxlcMjpk@redis-2276e2b-riteshbenjwal12-a2dc.a.aivencloud.com:11523";
+
+const pub = new Redis(REDIS_URL);
+const sub = new Redis(REDIS_URL);
 
 class SocketService {
   private _io: Server;
   constructor() {
     console.log("Init Socket Service...");
-    this._io = new Server();
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+
+    sub.subscribe("MESSAGES");
   }
 
   public initListeners() {
@@ -13,9 +27,18 @@ class SocketService {
     io.on("connect", (socket) => {
       console.log(`New Socket Connected`, socket.id);
 
-      socket.on("event:message", ({ message }) => {
+      socket.on("event:message", async ({ message }) => {
         console.log("New Message Rec. ", message);
+
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
       });
+    });
+
+    sub.on("message", async (channel, message) => {
+      if (channel === "MESSAGES") {
+        console.log("new message from redis", message);
+        io.emit("message", message);
+      }
     });
   }
 
